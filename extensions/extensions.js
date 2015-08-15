e5c865191b6e812c8aa91dce6ee82ec00ec6fa00
Colin McCloskey <colin.mccloskey@gmail.com>
2015-08-15 00:34:02 -0400

    register subservices with primary from extensions; map calls from known subservices

--- extensions/extensions.js
+++ extensions/extensions.js
@@ -1,12 +1,18 @@
 
 if (Meteor.isServer) {
 
+  // Connect back to the primary:
   var primary = Cluster.discoverConnection('primary');
 
-  Meteor.setInterval( function() {
-    var pingDate = primary.call("ping");
-    console.log("Pinging Primary:", pingDate);
-  }, 4000);
+  Meteor.setTimeout( function() {
+    primary.call("register", "ping", process.env.CLUSTER_SERVICE);
+    primary.call("register", "echo", process.env.CLUSTER_SERVICE);
+  }, 1000);
+
+  // Meteor.setInterval( function() {
+  //   var pingDate = primary.call("ping");
+  //   console.log("Pinging Primary:", pingDate);
+  // }, 15000);
 
   Meteor.methods({
     "ping": function() {
@@ -15,7 +21,7 @@ if (Meteor.isServer) {
     },
     "echo": function(message) {
       console.log("Extensions: Receiving Echo Request");
-      return "Echo: "+message;
+      return message;
     }
   });
 
--- primary/primary.js
+++ primary/primary.js
@@ -1,14 +1,30 @@
 
 if (Meteor.isServer) {
 
-  var extensions = Cluster.discoverConnection('extensions');
+  var servicesMap = {};
+  var serviceCall = function(serviceName, methodName /* arg1, arg2, ... */) {
+    // if (! _.isString(serviceName) ) return;
+    // if (! _.isString(methodName) ) return;
+    check(serviceName, String);
+    check(methodName, String);
+    console.log("Primary will make a call to Service:", serviceName, "Method:", methodName);
+    var service = servicesMap[serviceName];
+    if ( service ) {
+      console.log(" -> Will make call. Service", serviceName, "is defined.");
+      console.log(" -> Arguments to call:", [].slice.call(arguments, 2));
+      service.call(this, [].slice.call(arguments, 1));
+    } else
+      console.log(" -> Cannot make call. Service", serviceName, "not registered with primary");
+  };
+
+  // var extensions = Cluster.discoverConnection('extensions');
 
   Meteor.setInterval( function() {
-    var pingDate = extensions.call("ping");
-    var echoMsg = extensions.call("echo", "I Love You");
+    var pingDate = serviceCall('ping', 'ping'); // extensions.call("ping");
+    if (pingDate) console.log("Pinging Ext:", pingDate);
 
-    console.log("Pinging Ext:", pingDate);
-    console.log("Echoing Ext:", echoMsg);
+    var echoMsg = serviceCall('echo', 'echo', 'I Love You'); // extensions.call("echo", "I Love You");
+    if (echoMsg) console.log("Echoing Ext:", echoMsg);
   }, 5000);
 
   Meteor.methods({
@@ -16,6 +32,11 @@ if (Meteor.isServer) {
       console.log("Primary: Receiving Ping");
       return new Date();
     },
+    "register": function(serviceName, clusterConn) {
+      console.log("Subservice Registered:", serviceName, "at", clusterConn);
+      servicesMap[serviceName] = Cluster.discoverConnection(clusterConn);
+      console.log("Services Map now has", _.keys(servicesMap));
+    }
   });
 
 }
