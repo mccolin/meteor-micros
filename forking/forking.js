b20ab4b2006f833adf5bb271bb7ebc35d8a93126
Colin McCloskey <colin.mccloskey@gmail.com>
2015-12-01 14:10:58 -0500

    Fork node child worker processes from arbiter

--- forking/forking.js
+++ forking/forking.js
@@ -1,6 +1,9 @@
 
 if (Meteor.isServer) {
 
+  // Node Cluster: Process management, forking
+  var cluster = Meteor.npmRequire('cluster');
+
   var servicesMap = {};
 
   Meteor.methods({
@@ -16,7 +19,32 @@ if (Meteor.isServer) {
   });
 
   Meteor.startup(function () {
-    console.log("Arbiter has started.")
+
+    if (cluster.isMaster) {
+      console.log("Arbiter has started. PID:", process.pid, "Port:", process.env.PORT);
+
+      var numCPUs = Meteor.npmRequire('os').cpus().length;
+      var numWorkers = 2 || numCPUs;
+
+      for (var i = 1; i <= numWorkers; i++) {
+        var childPort = parseInt(process.env.PORT) + i;
+        console.log("Forking worker with child port", childPort, "...");
+        cluster.fork({'PORT': childPort});
+      }
+
+      cluster.on('online', function(worker) {
+        console.log("Worker", worker.process.pid, "has come online.");
+      });
+
+      cluster.on('exit', function(worker, code, signal) {
+        console.log("Worker", worker.process.pid, "has died.");
+      });
+
+    } else {
+      console.log("Worker has started. PID:", process.pid, "Port:", process.env.PORT);
+    }
+
+
   });
 
 }
